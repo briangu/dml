01b2755d6b540549d8d36ac39728fe547f3d9a96
Brian Guarraci <brian.guarraci@gmail.com>
2024-01-25 20:42:38 -0700

    update creds

--- deploy/docker/dml-mnist-cpu/train.py
+++ deploy/docker/dml-mnist-cpu/train.py
@@ -40,16 +40,16 @@ class MNISTNet(nn.Module):
     def __init__(self):
         super(MNISTNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
-        self.conv2 = nn.Conv2d(32, 20, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3)
         # self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(500, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc1 = nn.Linear(800, 64)
+        self.fc2 = nn.Linear(64, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
 #        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = x.view(-1, 500)
+        x = x.view(-1, 800)
         x = F.relu(self.fc1(x))
         # x = F.dropout(x, training=self.training)
         x = self.fc2(x)
