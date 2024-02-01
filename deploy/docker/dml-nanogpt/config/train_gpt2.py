df9b709073a800fcdd2d43f5dca094bbbef398da
Brian Guarraci <brian.guarraci@gmail.com>
2024-02-01 12:06:36 -0700

    update nanogpt

--- deploy/docker/dml-nanogpt/config/train_gpt2.py
+++ deploy/docker/dml-nanogpt/config/train_gpt2.py
@@ -2,14 +2,14 @@
 # launch as the following (e.g. in a screen session) and wait ~5 days:
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
-wandb_log = True
+wandb_log = False
 wandb_project = 'owt'
 wandb_run_name='gpt2-124M'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
 batch_size = 12
-block_size = 1024
+block_size = 256 # 1024
 gradient_accumulation_steps = 5 * 8
 
 # this makes total number of tokens be 300B
@@ -23,3 +23,8 @@ log_interval = 10
 
 # weight decay
 weight_decay = 1e-1
+
+# baby model
+n_layer = 6
+n_head = 8
+n_embd = 256
