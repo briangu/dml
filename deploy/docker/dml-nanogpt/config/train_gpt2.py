3401f53a53f53796b7e5e841d69ff41cdf81ec4f
Brian Guarraci <brian.guarraci@gmail.com>
2024-02-02 07:13:19 -0700

    add memory constraints

--- deploy/docker/dml-nanogpt/config/train_gpt2.py
+++ deploy/docker/dml-nanogpt/config/train_gpt2.py
@@ -1,6 +1,7 @@
 # config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
 # launch as the following (e.g. in a screen session) and wait ~5 days:
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+import os
 
 wandb_log = False
 wandb_project = 'owt'
@@ -8,9 +9,9 @@ wandb_run_name='gpt2-124M'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 12
-block_size = 256 # 1024
-gradient_accumulation_steps = 5 * 8
+batch_size = int(os.environ.get('MODEL_BATCH_SIZE', 12))
+block_size = int(os.environ.get('MODEL_BLOCK_SIZE', 512))
+gradient_accumulation_steps = 5 * int(os.environ['WORLD_SIZE'])
 
 # this makes total number of tokens be 300B
 max_iters = 600000
@@ -25,6 +26,6 @@ log_interval = 10
 weight_decay = 1e-1
 
 # baby model
-n_layer = 6
-n_head = 8
-n_embd = 256
+n_layer = int(os.environ.get('MODEL_NUM_LAYERS', 12))
+n_head = int(os.environ.get('MODEL_NUM_HEADS', 8))
+n_embd = int(os.environ.get('MODEL_EMBEDDING_DIM', 512))
